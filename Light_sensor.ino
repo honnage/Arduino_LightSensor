c5cccdc62c2b24c98110380455da3013a601e2f9
honnage <hon_da-1998@hotmail.com>
2020-11-11 18:38:44 +0700

    Update

--- Light_sensor.ino
+++ Light_sensor.ino
@@ -1,64 +1,52 @@
-int  val = 0 ;
+const int pingPin = 13;
+int inPin = 12;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(3, OUTPUT);
   pinMode(2, OUTPUT);
- 
-
+  pinMode(A0, OUTPUT);
 }
 
 void loop()
 {
-  
-  val = analogRead(5);    
-  Serial.print("val = "); // พิมพ์ข้อมความส่งเข้าคอมพิวเตอร์ "val = "
-  Serial.println(val); // พิมพ์ค่าของตัวแปร val
-  if(val >= 400){
-    digitalWrite(13, HIGH);
-    digitalWrite(12, HIGH);
-    digitalWrite(11, HIGH);
-    digitalWrite(10, HIGH);
- 
+  long duration, cm;
+  pinMode(pingPin, OUTPUT);
+  digitalWrite(pingPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin, LOW);
+  pinMode(inPin, INPUT);
+  duration = pulseIn(inPin, HIGH);
+  cm = microsecondsToCentimeters(duration);
+  if(cm < 50){
     digitalWrite(2, HIGH);
-    digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(7, HIGH);
-
-    delay(1000); // Wait for 100 millisecond(s)
-
-    digitalWrite(13, LOW);
-    delay(100); // Wait for 100 millisecond(s)
-
-    digitalWrite(12, LOW);
-    delay(100); // Wait for 100 millisecond(s)
- 
-    digitalWrite(11, LOW);
-    delay(100); // Wait for 100 millisecond(s)
-
-    digitalWrite(10, LOW);
-    delay(100); // Wait for 100 millisecond(s)
-
-    digitalWrite(4, LOW);
-    delay(100); // Wait for 100 millisecond(s)
-
-    digitalWrite(3, LOW);
-    delay(100); // Wait for 100 millisecond(s)
- 
+    digitalWrite(A0, 1);
+    
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+  }else{
     digitalWrite(2, LOW);
-    delay(100); // Wait for 100 millisecond(s)
+        
+    digitalWrite(A0, LOW);
+    digitalWrite(A0, 0);
 
-    digitalWrite(7, LOW);
-    delay(100); // Wait for 100 millisecond(s)
+    Serial.print(cm);
+    Serial.print("cm");
+    Serial.println();
+  } 
+ 
+ 
+    delay(1000);
 
-    delay(100); // Wait for 100 millisecond(s)
-  }
     
 }
+  long microsecondsToCentimeters(long microseconds)
+   {
+    // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+    // The ping travels out and back, so to find the distance of the
+    // object we take half of the distance travelled.
+    return microseconds / 29 / 2;
+  }
+ 
